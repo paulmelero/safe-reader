d8055d6e08d81a1a76a1fdb485a6260ff849258d
paulmelero <paul.melero@backmarket.com>
2026-02-05 21:27:59 +0100

    test: Update compat date in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'node:url';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2025-01-01',
   devtools: { enabled: true },
 
   devServer: {
